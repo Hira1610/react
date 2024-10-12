d2220db79671b288508d9579e7c0a9328fd85a6e
Hira1610 <hira97122@gmail.com>
2024-10-12 17:37:29 +0500

    Added

---
+++ src/App.jsx
@@ -0,0 +1,93 @@
+// import React from 'react'
+//  SIMPLE WAY BUT NOT EFFICENT
+// const App = () => {
+//   let user = "Hira";
+
+//   const changedUser = ()=>{
+//     console.log(user)
+//     user = "Hiruu"
+//     console.log(user)   //  yaha pr values to chnaged hori h but UI my show ni kr ri 
+//   }
+//   return (
+//     <div>
+//       <h1>Hello {user}</h1>    {/* // is sy ya hoga k jo upr user my value di hogi hmny wo yaha print hogi  */}
+//       <button onClick={changedUser}>User Changed </button>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+////USESTATE HOOKS
+// Hooks - special type of function 
+// import React, { useState } from 'react'
+
+// const App = () => {
+//   const [first, setfirst] = useState(0) ///is sy ya hoga k koi count start  hoga or ismy first jo h wo hmra readable h jo hmny pehli vlue initialize krwai h usko read krta ha or setfirst hmra writeable h jisky through hm apni values ya kuch b change krwa skty hai.
+//   return (
+//     <div>
+//       <h1>Number is {first}</h1>
+//       <button onClick={() => setfirst(first + 1)}>Increment</button>
+//       <button onClick={() => setfirst(first - 1)}>Decrement</button>
+//     </div>
+//   )
+// }
+// export default App
+
+
+//ADDING CSS AND FORM 
+// import React from 'react'
+
+// const App = () => {
+//   const submitHandler = (e)=>{
+//     e.preventDefault() //ya hmra page reload ni kry ga
+//     console.log("Hello")
+//   }
+//   return (
+//     <div>
+//       <form onSubmit={(e)=>{
+//         submitHandler(e)
+//       }}>
+//         <input className='px-4 py-3 rounded text-xl m-5 text-black font-semibold' type="text" placeholder='Enter your name' />
+//         <button className='px-4 py-3 m-6 rounded text-xl font-semibold bg-emerlad-500 text-white'> Submit</button>
+//       </form>
+//     </div>
+//   )
+// }
+// export default App
+
+
+///TWO WAY BINDING
+import React, { useState } from 'react'
+
+const App = () => {
+
+  const [username, setusername] = useState('')
+
+  const submitHandler = (e)=>{
+    e.preventDefault() //ya hmra page reload ni kry ga
+    console.log(username)
+    setusername('')
+  }
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+        submitHandler(e)
+      }}>
+        <input 
+        value={username}
+        onChange={(e)=>{
+          setusername(e.target.value)
+        }}
+        className='px-4 py-3 rounded text-xl m-5 text-black font-semibold' 
+        type="text" 
+        placeholder='Enter your name' />
+        <button className='px-4 py-3 m-6 rounded text-xl font-semibold bg-emerlad-500 text-white'> Submit</button>
+      </form>
+    </div>
+  )
+}
+export default App
+
